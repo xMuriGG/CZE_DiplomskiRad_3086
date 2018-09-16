acafff3522a624726357bf647918f027b84aa7ab
xMuriGG <muris.cengic@edu.fit.ba>
2018-09-16 18:51:43 +0200

    Visual adjustments.

--- CZE_DiplomskiRad_3086/Areas/Direktor/Models/ZaposlenikVMs.cs
+++ CZE_DiplomskiRad_3086/Areas/Direktor/Models/ZaposlenikVMs.cs
@@ -19,12 +19,14 @@ namespace CZE.Web.Areas.Direktor.Models
         {
             public int ZaposlenikId { get; set; }
             [Required]
+            [Display(Name = "Stepen obrazovanja")]
             public Zaposlenik.StepeniObrazovanja StepenObrazovanja { get; set; }
             [Required(ErrorMessage = "Broj lične karte je obavezno polje")]
-
+            [Display(Name = "Broj lične karte")]
             [StringLength(9, ErrorMessage = "Broj lične karte se mora sadržavati od 9 karaktera", MinimumLength = 9)]
             public string BrojLicneKarte { get; set; }
             [Required(ErrorMessage = "Broj računa je obavezno polje")]
+            [Display(Name = "Broj računa")]
             [MaxLength(20, ErrorMessage = "Maximalna dužina broja računa je 20 karaktera")]
             [MinLength(16, ErrorMessage = "Minimalna dužina broja računa je 16 karaktera")]
             public string BrojRacuna { get; set; }
